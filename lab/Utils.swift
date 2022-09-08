4661421a14a6886230f15c1ae19acf5fe5a1dc28
Azarattum <azarattum@pm.me>
2022-09-08 16:02:37 +0700

    Added docs

--- lab/Utils.swift
+++ lab/Utils.swift
@@ -1,9 +1,15 @@
 import Foundation
 
+/**
+ * Returns the minimum of `a` and `b`
+ */
 func mymin(_ a: Int, _ b: Int) -> Int {
     return a < b ? a : b;
 }
 
+/**
+ * Returns the maximum of `a` and `b`
+ */
 func mymax(_ a: Int, _ b: Int) -> Int {
     return a > b ? a : b;
 }
